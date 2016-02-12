1211857e9c6c95510d6ad63868d43e8c909e0cc7
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2016-02-12 09:31:46 +0200

    Another virty dirty solution would have been to create another method like User.create_without_admin_notification which would dynamically skip the call back to notify admins

--- rails_project/app/models/user.rb
+++ rails_project/app/models/user.rb
@@ -2,6 +2,14 @@ class User < ActiveRecord::Base
   after_create :send_welcome,
                :notify_admin_of_new_member
   attr_accessor :dont_send_to_admin?
+
+  def self.create_without_admin_notification!(params)
+    self.skip_callback :create, :after, :notify_admin_of_new_member
+    self.create!(params)
+    # Turn the callback back on
+    self.set_callback :create, :after, :notify_admin_of_new_member
+  end
+
   private
 
   def send_welcome
@@ -9,6 +17,6 @@ class User < ActiveRecord::Base
   end
 
   def notify_admin_of_new_member
-    Notifier.notify_admin_of_new_member(id) unless dont_send_to_admin?
+    Notifier.notify_admin_of_new_member(id)
   end
 end
